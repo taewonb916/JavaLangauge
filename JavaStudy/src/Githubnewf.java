b83480e1190c6ebaf4c7a92eb02f163d8adfd734
taewo <taewo@DESKTOP-4PFFJ2G>
2021-05-02 01:07:57 +0900

    패키지 업데이트 확인

--- JavaStudy/src/Githubnewf.java
+++ JavaStudy/src/Githubnewf.java
@@ -3,6 +3,7 @@ public class Githubnewf {
 	public static void main(String args[]) {
 		System.out.println("깃허브 자동 확인 용");
 		System.out.println("업데이트 되는지 확인용");
+		System.out.println("패키지에도 업데이트 되는지 확인용");
 	}
 	
 }
