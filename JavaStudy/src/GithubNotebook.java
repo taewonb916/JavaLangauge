bcc8ecc9e323ba151d20e2acfb038c36dffae3e5
taewo <taewo@DESKTOP-4PFFJ2G>
2021-05-02 01:13:12 +0900

    노트북 연동 확인완료

--- JavaStudy/src/GithubNotebook.java
+++ JavaStudy/src/GithubNotebook.java
@@ -3,6 +3,7 @@ class GithubNotebook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("노트북에서 연동되는지 확인");
+		System.out.println("노트북 연동 확인완료");
 	}
 
 }
